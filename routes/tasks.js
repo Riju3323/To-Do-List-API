f34f05579d237a33dcb803f7970d72b472a2e42a
Riju <87523667+Riju3323@users.noreply.github.com>
2022-07-01 12:15:01 +0530

    NaN

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,6 +3,14 @@ const app = express;
 
 const router = express.Router();
 
+const getAllTask = ((req,res) =>{
+  res.send('get all task items');
+});
+
+const createTask = ((req,res) => {
+  res.send('all items');
+});
+
 router.route('/').get((req,res)=>{
   res.send('All Items');
 });
