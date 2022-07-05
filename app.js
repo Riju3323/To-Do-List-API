c37a489006b020ec19e1c8bf4fb068a1819ba1e6
Riju <87523667+Riju3323@users.noreply.github.com>
2022-07-05 21:16:05 +0530

    NaN

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ let port = process.env.PORT || 3000;
 const start = async () => {
   try {
     await connectDB(process.env.mongolink);
-    app.listen(process.env.PORT, () =>
+    app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
   } catch (error) {
